39d6ece4dd17476715f746de3674ef5b2b3b3c27
robinsonking <878950187@qq.com>
2018-11-09 18:42:50 +0800

    commit support of mac's compilation.

--- allbuild.sh
+++ allbuild.sh
@@ -2,6 +2,7 @@
 
 chmod 777 autogen.sh
 chmod 777 share/genbuild.sh
+chmod 777 libsCopy.sh
 
 sudo apt-get install make  pkg-config autoconf libtool
 sudo apt-get install libboost-all-dev libssl-dev libevent-dev libdb-dev  libdb++-dev
@@ -10,12 +11,5 @@ sudo apt-get install libboost-all-dev libssl-dev libevent-dev libdb-dev  libdb++
 ./configure --with-incompatible-bdb --disable-tests --disable-bench
 make clean
 make
-
-cd src
-ar rc libomnicored.a omnicored-bitcoind.o omnicored-omnicoreApi.o 
-cp univalue/.libs/libunivalue.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
-cp crypto/libbitcoin_crypto.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
-cp leveldb/libleveldb.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
-cp leveldb/libmemenv.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
-cp secp256k1/.libs/libsecp256k1.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
-cp *.a  $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib -rf
+echo "copying ..."
+sh ./libsCopy.sh
