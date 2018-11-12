f796275e2f584cf567f914889ccc7d35a9cdbc49
robinsonking <878950187@qq.com>
2018-11-12 01:39:39 -0800

    commit sh files.

--- allbuild_mac.sh
+++ allbuild_mac.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #brew install automake berkeley-db libtool boost (miniupnpc) openssl pkg-config protobuf libevent
-#before running this file, you should install the pkgs above on mac os.
+#before running this file, you should install the pkgs above on mac os. 
+#if cmd "source libsCopy.sh" error,please run it manually.
 
 chmod 777 autogen.sh
 
@@ -15,3 +16,4 @@ sudo ar rc libomnicored.a omnicored-bitcoind.o omnicored-omnicoreApi.o
 cd ..
 
 source libsCopy.sh
+
--- libsCopy.sh
+++ libsCopy.sh
@@ -8,3 +8,4 @@ cp -rf leveldb/libleveldb.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib
 cp -rf leveldb/libmemenv.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib
 cp -rf secp256k1/.libs/libsecp256k1.a $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib
 cp -rf *.a  $GOPATH/src/github.com/HcashOrg/hcwallet/omnilib
+cd ..
